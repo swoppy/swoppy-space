8e681c38cf83248d8969862d0f8651d1d73669ee
swoppy <json.barba@gmail.com>
2020-07-01 17:32:15 +0800

    Added HTML event types

---
+++ src/ui/types/events.ts
@@ -0,0 +1,4 @@
+export type InputChangeEvent = React.ChangeEvent<HTMLInputElement>;
+export type ButtonClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>;
+export type FormSubmitEvent = React.FormEvent<HTMLFormElement>;
+export type DivKeyboardEvent = React.KeyboardEvent<HTMLDivElement>;
