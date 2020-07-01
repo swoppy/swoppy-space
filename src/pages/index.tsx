20147b568877213a4ea4425b2c80ddf9af419a04
swoppy <json.barba@gmail.com>
2020-07-01 14:03:01 +0800

    Updated index page: used ThemeSwitcher, used ThemeProvider, removed stock image reference

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,61 @@
 import React from "react"
 import { Link } from "gatsby"
-import Image from "../components/image"
 import SEO from "../components/seo";
 import "./css/default.css";
+import { IndexStore } from "./store/index_store";
+import { Theme, ThemedStyles, GlobalThemeStore, useStyles, ThemeProvider } from "../ui/themes";
+import { ThemeSwitcher } from "../components/theme_switcher/theme_switcher";
+import { Wrapper } from "../components/wrapper/wrapper";
+import { observer } from "mobx-react";
 
 // vscode unable to recognize import path for some reason, so ended up using require
 const baseStyles = require("./css/index.module.css");
 
-const IndexPage = () => (
+type IndexStyles = {
+  pageWrapper: string;
+  knobContainer: string;
+}
+
+const themedStyles: ThemedStyles<IndexStyles> = {
+  [Theme.LIGHT]: {
+    knobContainer: baseStyles.knobContainer,
+    pageWrapper: baseStyles.pageWrapper,
+  },
+  [Theme.DARK]: {
+    knobContainer: baseStyles.knobContainer,
+    pageWrapper: baseStyles.pageWrapper,
+  },
+};
+
+type IndexPageProps = {
+  store: IndexStore;
+};
+
+const BaseIndex = ({ store }: IndexPageProps) => {
+  const styles = useStyles(themedStyles);
+  return (
   <>
     <SEO title=""/>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p className={baseStyles.test}>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="donate">Go to donate page</Link> <br />
+    <Wrapper>
+      <div className={styles.knobContainer}>
+        <ThemeSwitcher store={store.theme}/>
+      </div>
+      <Link to="donate">Go to donate page</Link> <br />
+    </Wrapper>
   </>
-);
+  );
+};
+
+const IndexPage = observer(() => {
+  const [store] = React.useState(
+    new IndexStore(GlobalThemeStore.get() === Theme.DARK)
+  );
+  console.log(GlobalThemeStore.get());
+  return (
+    <ThemeProvider value={GlobalThemeStore.get()}>
+      <BaseIndex store={store}/>
+    </ThemeProvider>
+  );
+})
 
 export default IndexPage;
