68aa55d288f94e49caef10ff5ff44ecd93ff6045
swoppy <json.barba@gmail.com>
2020-07-03 19:12:17 +0800

    Updated intro text

--- src/pages/componentPages/text/home_text.ts
+++ src/pages/componentPages/text/home_text.ts
@@ -1,4 +1,4 @@
 export default {
-  intro1: `Hi, I'm swoppy I make stuff on the inter webs`,
-  intro2: `My favorite technologies right now are TypeScript, Reactjs, Gatsbyjs, Storybookjs and Nodejs`,
+  intro1: `Hi, I'm swoppy I make stuff on the interwebs.`,
+  intro2: `My favorite technologies right now are Reactjs, TypeScript, Gatsbyjs, Storybookjs and Nodejs`,
 }
