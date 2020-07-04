12bf15b213208085d47d76592d2a63183daaa11f
swoppy <json.barba@gmail.com>
2020-07-04 22:01:50 +0800

    :clown_face: Mock API
    Added placeholder data like API for project section

--- src/services/placeholder.ts
+++ src/services/placeholder.ts
@@ -1,26 +1,26 @@
 export const github = [
   {
-    name: 'website',
-    description: 'Personal website',
+    name: 'zcash-halving',
+    description: 'A Zcash halving countdown site, ETA November 17th based on mean block time of 75s, although in reality block times varies a lot. 🙃🧠🚀',
     siteLink: 'https://zcashblockhalf.com',
-    projectLink: 'github.com/swoppy',
+    projectLink: 'asdf',
+  },
+  {
+    name: 'zcash-halving-twit-bot',
+    description: 'A twitter bot that browse zcashblockhalf.com and takes a screenshot of the countdown at that point and tweets it with random catch phrases. 🔥🤖🔥',
+    siteLink: 'asdasd',
+    projectLink: 'asdf',
+  },
+  {
+    name: 'swoppy-space',
+    description: 'Personal website of Swoppy, built with Gatsby default starter and React 🤙😅',
+    siteLink: 'asdasd',
+    projectLink: 'asdf',
   },
   {
     name: 'website',
-    description: 'Personal website',
+    description: 'A go to repo for component ideas, has storybook setup on it 😎',
     siteLink: 'https://zcashblockhalf.com',
-    projectLink: 'github.com/swoppy',
-  },
-  {
-    name: 'website',
-    description: 'Personal website',
-    siteLink: 'https://zcashblockhalf.com',
-    projectLink: 'github.com/swoppy',
-  },
-  {
-    name: 'website',
-    description: 'Personal website',
-    siteLink: 'https://zcashblockhalf.com',
-    projectLink: 'github.com/swoppy',
+    projectLink: 'asdf',
   },
 ];
