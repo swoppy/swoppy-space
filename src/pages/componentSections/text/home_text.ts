074dbc8d60011a6ac6e5f5ac899875c1445c78fe
swoppy <json.barba@gmail.com>
2020-07-09 20:01:37 +0800

    :speech_balloon: Updated text
    Updated text files at intro

--- src/pages/componentSections/text/home_text.ts
+++ src/pages/componentSections/text/home_text.ts
@@ -1,4 +1,4 @@
 export default {
-  intro1: `Hi, I'm swoppy I make stuff on the interwebs.`,
-  intro2: `My favorite technologies right now are Reactjs, TypeScript, Gatsbyjs, Storybookjs and Nodejs`,
+  intro1: `Hi, I'm swoppy and I make stuff on the interwebs.`,
+  intro2: `My favorite technologies right now are Reactjs, TypeScript, Gatsbyjs, Storybookjs and Nodejs.`,
 }
