5470ab7e3e615b1bfdc64d58e81c48d68536ece6
swoppy <json.barba@gmail.com>
2020-07-12 19:41:43 +0800

    :recycle: Refactored component
    Updated component and used font from asset

--- src/pages/componentSections/intro.tsx
+++ src/pages/componentSections/intro.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Theme, ThemedStyles, useStyles } from "../../ui/themes";
-import { ThemeSwitcher } from "../../components";
-import { ThemeStore } from '../store/theme_store';
-import text from './text/home_text';
+import React from "react"
+import { Theme, ThemedStyles, useStyles } from "../../ui/themes"
+import { ThemeSwitcher } from "../../components"
+import { ThemeStore } from "../store/theme_store"
+import text from "./text/home_text"
+import { IntroIsometric } from "../../components/isometric_art/intro_isometric"
 // vscode unable to recognize import path for some reason, so ended up using require
-const baseStyles = require("../css/intro.module.css");
+const baseStyles = require("../css/intro.module.css")
 
 type IntroStyles = {
-  pageWrapper: string;
-  indexContainer: string;
-  knobContainer: string;
-  greetingsContainer: string;
+  pageWrapper: string
+  indexContainer: string
+  knobContainer: string
+  wrapper: string
+  greetingsContainer: string
+  square: string
 }
 
 const themedStyles: ThemedStyles<IntroStyles> = {
@@ -18,49 +21,60 @@ const themedStyles: ThemedStyles<IntroStyles> = {
     indexContainer: baseStyles.indexContainer,
     knobContainer: baseStyles.knobContainer,
     pageWrapper: baseStyles.pageWrapper,
+    wrapper: baseStyles.wrapper,
     greetingsContainer: baseStyles.lightGreetingsContainer,
+    square: baseStyles.square,
   },
   [Theme.DARK]: {
     indexContainer: baseStyles.indexContainer,
     knobContainer: baseStyles.knobContainer,
     pageWrapper: baseStyles.pageWrapper,
+    wrapper: baseStyles.wrapper,
     greetingsContainer: baseStyles.darkGreetingsContainer,
+    square: baseStyles.square,
   },
-};
+}
 
 type ThemeSectionProps = {
-  store: ThemeStore;
-};
+  store: ThemeStore
+}
 
 const ThemeSection = ({ store }: ThemeSectionProps) => {
-  const styles = useStyles(themedStyles);
+  const styles = useStyles(themedStyles)
   return (
     <div className={styles.knobContainer}>
-      <ThemeSwitcher store={store.theme}/>
+      <ThemeSwitcher store={store.theme} />
     </div>
-  );
-};
+  )
+}
 
-const IntroText = () => {
-  const styles = useStyles(themedStyles);
+const IntroSection = () => {
+  const styles = useStyles(themedStyles)
   return (
-    <div className={styles.greetingsContainer}>
-      <h1>{text.intro1}</h1>
-      <h4><p>{text.intro2}</p></h4>
+    <div className={styles.wrapper}>
+      <div className={styles.greetingsContainer}>
+        <h1>{text.intro1}</h1>
+        <h4>
+          <p>{text.intro2}</p>
+        </h4>
+      </div>
+      <div className={styles.square}>
+        <IntroIsometric />
+      </div>
     </div>
-  );
-};
+  )
+}
 
 type IntroProps = {
   store: ThemeStore
-};
+}
 
 export const Intro = ({ store }: IntroProps) => {
-  const styles = useStyles(themedStyles);
+  const styles = useStyles(themedStyles)
   return (
     <div className={styles.indexContainer}>
-      <ThemeSection store={store}/>
-      <IntroText/>
+      <ThemeSection store={store} />
+      <IntroSection />
     </div>
-  );
-};
+  )
+}
