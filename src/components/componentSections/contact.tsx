fc5c69cdf62f3639eb71995db01f6789ac7d394b
swoppy <json.barba@gmail.com>
2020-07-15 17:55:37 +0800

    :lipstick: Added contact section
    Created tsx, text file and css module for contact section

---
+++ src/components/componentSections/contact.tsx
@@ -0,0 +1,57 @@
+import React from "react"
+import { ThemedStyles, Theme, useStyles } from "../../ui/themes"
+import text from "./text/contact_text"
+import { ExternalLink } from "../external_link/external_link"
+const baseStyles = require("../../css/contact.module.css")
+
+type GetInTouchStyles = {
+  container: string
+  closingSection: string
+  getInTouch: string
+  footer: string
+  link: string
+}
+
+const themedStyles: ThemedStyles<GetInTouchStyles> = {
+  [Theme.LIGHT]: {
+    container: baseStyles.container,
+    closingSection: baseStyles.lightClosingSection,
+    getInTouch: baseStyles.lightGetInTouch,
+    footer: baseStyles.lightFooter,
+    link: baseStyles.link,
+  },
+  [Theme.DARK]: {
+    container: baseStyles.container,
+    closingSection: baseStyles.darkClosingSection,
+    getInTouch: baseStyles.darkGetInTouch,
+    footer: baseStyles.darkFooter,
+    link: baseStyles.link,
+  },
+}
+
+export const Contact = () => {
+  const styles = useStyles(themedStyles)
+  return (
+    <div className={styles.container}>
+      <div className={styles.closingSection}>
+        <div className={styles.getInTouch}>
+          <h2>{text.closing}</h2>
+          <h4>
+            {text.contacts}
+            <ExternalLink to={text.emailAddress} className={styles.link}>
+              {text.email}
+            </ExternalLink>
+            {text.or}
+            <ExternalLink to={text.twitterAddress} className={styles.link}>
+              {text.twitter}
+            </ExternalLink>
+          </h4>
+        </div>
+        <div className={styles.footer}>
+          <div>{text.footer.copyRight}</div>
+          <div>{text.footer.builtWith}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/componentSections/text/contact_text.ts
@@ -0,0 +1,13 @@
+export default {
+  closing: `Get in touch 🤙`,
+  contacts: `Say Hi via `,
+  email: `hey@swoppy.space`,
+  twitter: `Twitter`,
+  or: ` or `,
+  emailAddress: `mailto:hey@swoppy.space`,
+  twitterAddress: `https://twitter.com/swoppy_`,
+  footer: {
+    copyRight: `© 2020 Swoppy`,
+    builtWith: `Built with React and Gatsby`,
+  },
+}
