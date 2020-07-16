ba4a8f88442a733ef2c4ac02d6dc0c10e7472783
swoppy <json.barba@gmail.com>
2020-07-16 12:58:29 +0800

    :label: Added type
    Added type mouseEvent type for html div

--- src/ui/types/events.ts
+++ src/ui/types/events.ts
@@ -1,4 +1,5 @@
-export type InputChangeEvent = React.ChangeEvent<HTMLInputElement>;
-export type ButtonClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>;
-export type FormSubmitEvent = React.FormEvent<HTMLFormElement>;
-export type DivKeyboardEvent = React.KeyboardEvent<HTMLDivElement>;
+export type InputChangeEvent = React.ChangeEvent<HTMLInputElement>
+export type ButtonClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>
+export type FormSubmitEvent = React.FormEvent<HTMLFormElement>
+export type DivKeyboardEvent = React.KeyboardEvent<HTMLDivElement>
+export type DivMouseEvent = React.MouseEvent<HTMLDivElement, MouseEvent>
