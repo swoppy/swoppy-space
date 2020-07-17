d1c96e73a6118c2fe50540c7af0f83e1629210fb
swoppy <json.barba@gmail.com>
2020-07-17 13:26:28 +0800

    :chart_with_upwards_trend: Added site verification
    Added google site verification

--- src/components/performance_link/performance_link.tsx
+++ src/components/performance_link/performance_link.tsx
@@ -4,6 +4,10 @@ import { Helmet } from "react-helmet"
 export const PerformanceLink = () => {
   return (
     <Helmet>
+      <meta
+        name="google-site-verification"
+        content="Wf1Feal8nPxTK4A_MSa2SKbZgwmmD95fnQmCp9tmuvs"
+      />
       <link rel="preconnect" href="https://api.github.com" />
     </Helmet>
   )
