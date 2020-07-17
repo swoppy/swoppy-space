00df3121d266210f2c65788e97c6cc79e6300fdb
swoppy <json.barba@gmail.com>
2020-07-17 18:51:45 +0800

    :fire: Removed meta
    Removed meta for google site verification

--- src/components/performance_link/performance_link.tsx
+++ src/components/performance_link/performance_link.tsx
@@ -4,10 +4,6 @@ import { Helmet } from "react-helmet"
 export const PerformanceLink = () => {
   return (
     <Helmet>
-      <meta
-        name="google-site-verification"
-        content="Wf1Feal8nPxTK4A_MSa2SKbZgwmmD95fnQmCp9tmuvs"
-      />
       <link rel="preconnect" href="https://api.github.com" />
     </Helmet>
   )
